ccb699c977a13b3fd86cc31eb7af9a3fb393c50b
RatamaG <rafaeltamarag1@gmail.com>
2023-10-09 11:46:52 -0500

    first commit

---
+++ e2e/example.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('link', { name: '/admins/', exact: true }).click();
+  await page.locator('html').click();
+  await page.getByRole('link', { name: 'View' }).click();
+  await page.getByRole('link', { name: 'Back to all Admins' }).click();
+
+  const newURL = page.url();
+  const response = await page.goto(newURL);
+  if (response !== null) {
+      const statusCode = response.status();
+      expect(statusCode).toBe(200);
+  }
+});
---
+++ models/admin.go
@@ -0,0 +1,58 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
+	"github.com/gofrs/uuid"
+)
+
+// Admin is used by pop to map your admins database table to your go code.
+type Admin struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
+	Role      string    `json:"role" db:"role"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (a Admin) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Admins is not required by pop and may be deleted
+type Admins []Admin
+
+// String is not required by pop and may be deleted
+func (a Admins) String() string {
+	ja, _ := json.Marshal(a)
+	return string(ja)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (a *Admin) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: a.Email, Name: "Email"},
+		&validators.StringIsPresent{Field: a.Password, Name: "Password"},
+		&validators.StringIsPresent{Field: a.Role, Name: "Role"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (a *Admin) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (a *Admin) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
